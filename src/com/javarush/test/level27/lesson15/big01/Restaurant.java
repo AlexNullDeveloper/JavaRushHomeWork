244becd07ec66e2918134f4faf268657084134e6
AlexDevNull <talismanoff1990@yandex.ru>
2016-12-09 20:47:31 +0300

    friday fun ^_^

--- src/com/javarush/test/level27/lesson15/big01/Restaurant.java
+++ src/com/javarush/test/level27/lesson15/big01/Restaurant.java
@@ -1,10 +1,16 @@
 package com.javarush.test.level27.lesson15.big01;
 
-/**
- * Created by Александр on 28.11.2016.
- */
+import com.javarush.test.level27.lesson15.big01.kitchen.Cook;
+import com.javarush.test.level27.lesson15.big01.kitchen.Waitor;
+
 public class Restaurant {
     public static void main(String[] args) {
+        Waitor waitor = new Waitor();
+        Cook cook = new Cook("Amigo");
+        cook.addObserver(waitor);
 
+        Tablet tablet = new Tablet(5);
+        tablet.addObserver(cook);
+        tablet.createOrder();
     }
 }
--- src/com/javarush/test/level27/lesson15/big01/kitchen/Dish.java
+++ src/com/javarush/test/level27/lesson15/big01/kitchen/Dish.java
@@ -2,13 +2,27 @@ package com.javarush.test.level27.lesson15.big01.kitchen;
 
 import java.util.Arrays;
 
-/**
- * Created by Александр on 28.11.2016.
- */
-public enum  Dish {
-    Fish, Steak, Soup, Juice, Water;
+
+public enum Dish {
+    Fish(25),
+    Steak(30),
+    Soup(15),
+    Juice(5),
+    Water(3);
+
+    private int duration;
+    private Dish(int duration) {
+        this.duration = duration;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
 
     public static String allDishesToString() {
-        return values().length == 0 ? "" : Arrays.toString(values()).substring(1, Arrays.toString(values()).length() - 1);
+        StringBuilder sb = new StringBuilder(Arrays.toString(values()));
+        sb.delete(sb.length() - 1, sb.length());
+        sb.delete(0, 1);
+        return sb.toString();
     }
 }
